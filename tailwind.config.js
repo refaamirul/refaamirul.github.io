36a8a0ef7bfc9bcdb75842672eb0d0883cbe943c
refaamirul <andimochrefa@gmail.com>
2023-01-03 08:30:16 +0800

    menambahkan dec di project clock analog

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         primary: '#14b8a6',
         secondary: '#64748b',
         dark: '#0f172a',
+        wa: '#25d366',
+        red: '#ff0000',
+        ins: '#E4405F',
+        twt: '1DA1F2',
+        lin: '#0A66C2',
       },
       screens: {
         '2xl': '1320px',
